aa9695fd063eff4831894ea68bc1c691f300466e
ajf412 <aaronjfaulkner@yahoo.com>
2019-02-04 22:11:36 -0500

    Audio files added.  Prefabs created.  Audio now plays on collision, and on block destruction.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,21 +8,26 @@ public class Ball : MonoBehaviour
     // Configuration Parameters
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f, yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
     // State
     Vector2 paddleToBallVector;
     bool hasStarted = false;
 
+    //Cached Component References
+    AudioSource myAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hasStarted == false)
+        if (!hasStarted)
         {
             LockBallToPaddle();
             LaunchOnMouseClick();
@@ -43,5 +48,15 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+        
     }
 }
--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    // Configuration Parameters
+    [SerializeField] AudioClip[] explosions;
+
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Ball")
         {
+            AudioClip clip = explosions[UnityEngine.Random.Range(0, explosions.Length)];
+            AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
             Destroy(gameObject);
         }
         
